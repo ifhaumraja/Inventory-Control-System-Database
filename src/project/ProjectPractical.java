073da8ce46f70944c16062e6edad4eeaaad8ff7f
ifhaumraja <116719383+ifhaumraja@users.noreply.github.com>
2023-01-11 03:40:10 +0530

    Add files via upload

---
+++ src/project/ProjectPractical.java
@@ -0,0 +1,13 @@
+package project;
+
+
+public class ProjectPractical {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
